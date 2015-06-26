007d664e2f30ab82a34565e22e236f039be3befe
Franco Viola <franco.viola@live.com>
2015-06-26 16:57:36 -0400

    Fix up results page

--- server.rb
+++ server.rb
@@ -2,21 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 require 'imdb'
-
-# i = Imdb::Movie.new("0095016")
-
-# p i.title
-# #=> "Die Hard"
-
-# p i.cast_members.first
-# #=> "Bruce Willis"
+enable :sessions
 
 get '/home' do
 	erb :movie_search
 end
 
 post '/posters' do
+	@random = rand(9)
 	@search = Imdb::Search.new(params[:movie_search])
-
+	@year = @search.movies[@random].year
+	@title = @search.movies[@random].title
+	session[:year] = @year
+	session[:title] = @title
 	erb :trivia
 end 
+
+post '/results' do
+	@answer = params[:answer]
+	@i = Imdb::Movie.new(@answer)
+	@year_answer = session[:year]
+	@title_answer = session[:title]
+
+	erb :results
+end
